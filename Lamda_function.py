c6f39be19ad979ede40467e4d1fadfb54da5bbf3
Ahmad87505 <mmffcc03@gmail.com>
2025-08-02 13:07:54 +0500

    Add files via upload

---
+++ Lamda_function.py
@@ -0,0 +1,13 @@
+add = lambda x, y: x + y
+print(add(3, 5))  # Output: 8
+
+cube= lambda x: x*x*x
+print(cube(3))
+#Using def method
+def add(x, y):
+    return x + y
+
+
+add_lambda = lambda x, y: x + y
+
+print(add_lambda(3, 5))
