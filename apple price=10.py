562f19c2bf162c0455c475971ba6925164ab6359
Ahmad87505 <mmffcc03@gmail.com>
2025-08-02 12:36:43 +0500

    Add files via upload

---
+++ apple price=10.py
@@ -0,0 +1,8 @@
+appleprice= 10
+bugget= 200
+if(appleprice-bugget>50):
+    print("Alexa,buy 10 apples")
+elif (appleprice-bugget>70):
+     print("Alexa,buy 20 apples")
+else:
+    print("Alexa,cannot buy apples")
