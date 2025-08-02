88aa6b2f2de967ac50ff97d78f59a2869581df4e
Ahmad87505 <mmffcc03@gmail.com>
2025-08-02 12:56:35 +0500

    Add files via upload

---
+++ milies into km.py
@@ -0,0 +1,6 @@
+kilometer=float(input("Enter the value in km: "))
+#Here float is used to store the decimal value
+miles=kilometer* 0.621371
+# 1mile is equal to 0.621371
+print("Km into miles",miles)
+
